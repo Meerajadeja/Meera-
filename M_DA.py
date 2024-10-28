3fc5230a88f36d38ecb9067be1479771a122df5e
Meerajadeja <meeranimavat62@gmail.com>
2024-10-28 20:09:33 +0000

    Update M_DA.py

--- M_DA.py
+++ M_DA.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import streamlit as st
-st.title ("First Analytics App")
+st.title ("My First Analytics App")
 df = pd.read_csv("melbourne (1).csv")
 st.dataframe(Df)
 import pandas as pd
