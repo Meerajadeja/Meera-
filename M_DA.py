03c5291bd5a579e43d9ea700145d482682770d9c
Meerajadeja <meeranimavat62@gmail.com>
2024-10-28 20:17:44 +0000

    Update M_DA.py

--- M_DA.py
+++ M_DA.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import streamlit as st
 st.title ("My First Analytics App")
-df = pd.read._csv("C:\Users\Meera Nimavat\OneDrive\Desktop\melbourne (1).csv")
+data_url = pd.read._csv("C:\Users\Meera Nimavat\OneDrive\Desktop\melbourne (1).csv")
 st.dataframe(Df)
 import pandas as pd
 
